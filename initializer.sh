b8dfbd2540eaccb3acb4a67e2e149029ea0c66b8
JAYESH Kumar <jayeshkumar044@gmail.com>
2021-01-10 04:02:08 +0530

    Added script to run as sudo user

--- initializer.sh
+++ initializer.sh
@@ -1,2 +1,8 @@
-pip install flask || pip3 install flask
-pip install flask_cors || pip3 install falsk 
+{
+    pip install flask || pip3 install flask
+    pip install flask_cors || pip3 install falsk 
+}||{
+    echo "Running as sudo"
+    sudo pip install flask || sudo pip3 install flask
+    sudo pip install flask_cors || sudo pip3 install falsk    
+}
